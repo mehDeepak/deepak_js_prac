900d0d86a1a1a94da38cc6634722a1a48da428ae
mehDeepak <154570491+mehDeepak@users.noreply.github.com>
2024-01-01 04:10:20 +0000

    02 js

---
+++ 02_basic_js/02_array.js
@@ -0,0 +1,7 @@
+let mycolor = ["red","blue","green"];
+// console.log(mycolor);
+// console.log(mycolor[0]);
+// console.log(mycolor.length);
+let yourColor = ["white","skyblue","black"]
+mycolor.push(yourColor);
+console.log(newColor);
