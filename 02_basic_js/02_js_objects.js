4971b241b4bb0370877a08c0555e7ccf689ddd0e
mehDeepak <154570491+mehDeepak@users.noreply.github.com>
2024-01-02 07:18:16 +0000

    edited

--- 02_basic_js/02_js_objects.js
+++ 02_basic_js/02_js_objects.js
@@ -1,10 +1,18 @@
+
+//non-single ton object
 let myObj = {
     name:"deepak",
+    fistName: "Emi",
+    LastName : "Damn",
     age:30,
     nationality: "Nepali",
-    "my class" : 9
+    "my class" : 9,
+    myFullName: function(){
+        return this.fistName + " " + this.LastName
+    }
    
 }
+console.log(myObj.myFullName());
 
 // console.log(myObj.name);
 myObj.newfunc= function(){
@@ -12,6 +20,23 @@ myObj.newfunc= function(){
 }
 myObj.newfunc();
 // console.log(myObj["my class"]);
+//single ton object
+const newUser = new Object();
+newUser.id = "abc12"
+newUser.name = "Deepak"
+newUser.isLoggedIN = true
+// console.log(newUser);
+// console.log(newUser.name);
+const userdata = {
+    fullname:{
+        username:{
+            firstName:"deepak",
+            lastName:"Bist"
+        }
+    }
+}
+console.log(userdata.fullname.username.firstName);
+
 
 
 
