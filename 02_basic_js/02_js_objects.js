ae7b9c5f76ed034b0888ecab9dc4bd1e5f8b3958
mehDeepak <154570491+mehDeepak@users.noreply.github.com>
2024-01-02 02:17:47 +0000

    js object part i

---
+++ 02_basic_js/02_js_objects.js
@@ -0,0 +1,18 @@
+let myObj = {
+    name:"deepak",
+    age:30,
+    nationality: "Nepali",
+    "my class" : 9
+   
+}
+
+// console.log(myObj.name);
+myObj.newfunc= function(){
+    // console.log(`my name is ${this.name} and i am ${this.age} year old`);
+}
+myObj.newfunc();
+// console.log(myObj["my class"]);
+
+
+
+
